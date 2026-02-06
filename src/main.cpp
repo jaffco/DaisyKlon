4aa3d6bb45967f442168cb1667d7ce398b929a79
Joel A. Jaffe <joel@jaffesd.com>
2026-02-06 12:01:41 -0800

    Portaklon for Daisy

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,50 @@
 #include "../libDaisy/src/daisy_seed.h"
+#include "../Portaklon/Portaklon.hpp"
 
 using namespace daisy;
 
 static DaisySeed hardware;
+static Portaklon portaklon;
+
+void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
+{
+    // Use left input channel for mono processing
+    float monoInput[128];
+    for (size_t i = 0; i < size; ++i) {
+        monoInput[i] = in[0][i];
+    }
+
+    float monoOutput[128];
+    portaklon.process(monoInput, monoOutput, size);
+
+    // Output to both channels
+    for (size_t i = 0; i < size; ++i) {
+        out[0][i] = monoOutput[i];
+        out[1][i] = monoOutput[i];
+    }
+}
 
 int main() {
   hardware.Init();
 
+  // Set audio parameters
+  hardware.SetAudioBlockSize(128);
+  hardware.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
+
+  // Initialize Portaklon
+  portaklon.init(48000, 128);
+
+  // Start audio
+  hardware.StartAudio(AudioCallback);
+
   hardware.StartLog(); 
-  // hardware.StartLog(true); // waits for connection before proceeding
-
-  System::Delay(200);
   hardware.PrintLine("===========================================");
-  hardware.PrintLine("                It's alive!                ");
+  hardware.PrintLine("          Portaklon Integrated!            ");
   hardware.PrintLine("===========================================");
-  hardware.PrintLine("");
 
-  // blink
+  // Main loop
   while (true) {
-    static bool ledState = false;
-    ledState = !ledState;
-    hardware.SetLed(ledState);
-    hardware.PrintLine("LED is now %s", ledState ? "ON" : "OFF");
-    System::Delay(500);
+    System::Delay(1000);
   }
 
   return 0;
